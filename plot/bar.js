29fcc45d13f322e4700f5f9e467c2c07c19f4311
tlacher1 <35156458+tlacher1@users.noreply.github.com>
2023-01-30 16:30:51 -0500

    Update bar.js

--- plot/bar.js
+++ plot/bar.js
@@ -1,26 +1,48 @@
 console.log(data)
-
 // country names
 countries = data.map(function (row){
     return row.country
 });
-
 // trace for inflation data
 let trace = {
     x: data.map(row => row.country),
     y: data.map(row => row.inflation_rate),
     type: "bar"
 };
-
 // data trace array
 let traceData = [trace];
-
 // apply the group barmode to the layout
-let layout = {
-    title: "2021 Inflation Rates by Country"
-};
-
-// render the plot to the div tag with id
+var layout = {
+    xaxis: {
+      position: "bottom",
+      titlefont: {
+        family: 'Arial, sans-serif',
+        size: 18,
+        color: 'black'
+      },
+      showticklabels: true,
+      tickangle: 'auto',
+      tickfont: {
+        family: 'Arial, sans-serif',
+        size: 14,
+        color: 'black'
+      },
+    },
+    yaxis: {
+      title: 'Inflation Rate (Percentage)',
+      titlefont: {
+        family: 'Arial, sans-serif',
+        size: 18,
+        color: 'black'
+      },
+      showticklabels: true,
+      tickangle: 45,
+      tickfont: {
+        family: 'Arial, sans-serif',
+        size: 14,
+        color: 'black'
+      },
+    }
+  };
 Plotly.newPlot("plot", traceData, layout);
-
 
